654cceadd42adf8eaeb2c3ba534c490a116ab523
ryan <al_tair@live.com>
2026-04-01 00:24:02 +0800

    init

---
+++ example/win32_websocket_example.dart
@@ -0,0 +1,6 @@
+import 'package:win32_websocket/win32_websocket.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/win32_websocket_test.dart
@@ -0,0 +1,16 @@
+import 'package:win32_websocket/win32_websocket.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
